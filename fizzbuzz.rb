b73505f053f165e9d41628660f17f851401cbfaf
Sarah Jacob <smjacob91@gmail.com>
2018-07-03 11:22:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,33 +4,20 @@ def fizzbuzz(int)
   end
 end
 
-
-
-
-
-
-
-
-
-describe "fizzbuzz" do
-  it 'returns "Fizz" when the number is divisible by 3' do
-    fizz_3 = fizzbuzz(3)
-
-    expect(fizz_3).to eq("Fizz")
-  end
-  it 'returns "Buzz" when the number is divisible by 5' do
-    fizz_5 = fizzbuzz(5)
-
-    expect(fizz_5).to eq("Buzz")
-  end
-  it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
-    fizz_15 = fizzbuzz(15)
-
-    expect(fizz_15).to eq("FizzBuzz")
-  end
-  it 'returns nil when the number is not divisible by 3 or 5' do
-    fizz_4 = fizzbuzz(4)
-
-    expect(fizz_4).to eq(nil)
+def fizzbuzz(int)
+  if int % 5 == 0
+    return "Buzz"
   end
 end
+
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  end
+end
+
+
+
+
+
+
