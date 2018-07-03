b3d44d54509a8a98ada15d7b74a32728c777bdf8
Sarah Jacob <smjacob91@gmail.com>
2018-07-03 11:28:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,15 +2,7 @@ def fizzbuzz(int)
   if int % 3 == 0
     return "Fizz"
   end
-  elsif int % 5 == 0
-    return "Buzz"
-  end
-  else int % 3 == 0 && int % 5 == 0
-    return "FizzBuzz"
 end
-
-
-
 
 
 
